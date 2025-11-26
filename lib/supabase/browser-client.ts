dc4562d6ae894be00023b986774dd1712309a4ea
Larry <L.abbo@yahoo.com>
2025-11-26 16:53:32 +0200

    Update browser-client.ts

--- lib/supabase/browser-client.ts
+++ lib/supabase/browser-client.ts
@@ -1,9 +1,11 @@
+// lib/supabase/browser-client.ts
+
 "use client";
 
-import { createBrowserClient } from "@supabase/ssr";
+import { createBrowserClient as createSupabaseBrowserClient } from "@supabase/ssr";
 
-export function createBrowserSupabaseClient() {
-  return createBrowserClient(
+export function createBrowserClient() {
+  return createSupabaseBrowserClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
