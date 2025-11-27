f4998363620a9273bef246ec990e0f5919640521
Larry <L.abbo@yahoo.com>
2025-11-27 18:23:00 +0200

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,26 +1,23 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
     extend: {
       colors: {
-        havenly: {
-          bg: "#020617",
-          card: "#020617",
-          accent: "#34d399",
-        },
-      },
-      borderRadius: {
-        xl: "1.25rem",
-        "2xl": "1.5rem",
-      },
-    },
+        brand: {
+          primary: "#4CA7A3",
+          light: "#76C5C0",
+          dark: "#2E837F"
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 };
 
 export default config;
