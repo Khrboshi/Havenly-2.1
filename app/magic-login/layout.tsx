988d309becc8930f19c0569fe9e4d618b70011d6
Larry <L.abbo@yahoo.com>
2025-12-04 14:49:41 +0200

    Create layout.tsx

---
+++ app/magic-login/layout.tsx
@@ -0,0 +1,9 @@
+export default function LoginLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className="bg-slate-950 text-white antialiased">
+        {children}
+      </body>
+    </html>
+  );
+}
