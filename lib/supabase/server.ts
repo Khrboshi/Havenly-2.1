85fcd69b2c18f263922f6b6ab83b8d70b01b40a9
Larry <L.abbo@yahoo.com>
2025-12-12 12:02:02 +0200

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -9,13 +9,8 @@ export function createServerSupabase() {
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
       cookies: {
-        getAll() {
-          return cookieStore.getAll();
-        },
-        setAll(cookies) {
-          cookies.forEach(({ name, value, options }) => {
-            cookieStore.set(name, value, options);
-          });
+        get(name) {
+          return cookieStore.get(name)?.value;
         },
       },
     }
