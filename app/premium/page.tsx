cb882b1fb2c2e73155f5d859ed76b3883567611a
Larry <L.abbo@yahoo.com>
2025-12-15 17:55:26 +0200

    Delete app/premium directory

--- app/premium/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation";
-
-/**
- * Deprecated route.
- * Premium page has been consolidated under /(public)/premium
- */
-export default function DeprecatedPremiumPage() {
-  redirect("/premium");
-}
