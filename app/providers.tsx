dbdc8bcaa6668a6ef2d10fb2de2e08bc07da392c
Larry <L.abbo@yahoo.com>
2026-02-13 23:59:24 +0200

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,11 +1,17 @@
 // app/providers.tsx
 "use client";
+
+import React from "react";
+import { SupabaseSessionProvider } from "@/components/SupabaseSessionProvider";
+import ServiceWorkerRegister from "@/components/ServiceWorkerRegister";
 import InstallPrompt from "@/components/InstallPrompt";
-// ...
-return (
-  <SupabaseSessionProvider>
-    <ServiceWorkerRegister />
-    <InstallPrompt />
-    {children}
-  </SupabaseSessionProvider>
-);
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <SupabaseSessionProvider>
+      <ServiceWorkerRegister />
+      <InstallPrompt />
+      {children}
+    </SupabaseSessionProvider>
+  );
+}
