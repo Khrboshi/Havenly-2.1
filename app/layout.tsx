4065c0e7b0b5c3554a9c0d225c1ddf4fc6e8a699
Larry <L.abbo@yahoo.com>
2026-02-17 17:49:11 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,8 +23,8 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body>
+        <DeepLinkBootstrap />
         <Providers>
-          <DeepLinkBootstrap />
           <Navbar />
           {children}
           <InstallPrompt />
