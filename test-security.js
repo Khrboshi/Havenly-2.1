1bc09ee0f42c3891fd8750d0a9ee7994490d6fbb
Larry <L.abbo@yahoo.com>
2026-03-26 10:22:17 +0200

    test: add security test file

---
+++ test-security.js
@@ -0,0 +1,8 @@
+// Test file for Bito AI Code Review
+const API_KEY = "sk-1234567890abcdef1234567890abcdef";
+const DATABASE_PASSWORD = "admin123456";
+
+function processUserData(userInput) {
+    eval(userInput);
+    return true;
+}
