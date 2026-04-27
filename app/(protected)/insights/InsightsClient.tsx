5a4e7f7c6b72ba11d62a5bc053718392d6f33077
Larry <L.abbo@yahoo.com>
2026-04-27 16:34:56 +0300

    chore: update InsightsClient.tsx

--- app/(protected)/insights/InsightsClient.tsx
+++ app/(protected)/insights/InsightsClient.tsx
@@ -650,7 +650,7 @@ function WeeklySummarySection({ hasRealData }: { hasRealData: boolean }) {
 
           <div className="flex items-center gap-3 pt-1 border-t border-qm-border-subtle">
             <span className="text-xs text-qm-faint" suppressHydrationWarning>
-              Last updated {generatedLabel}
+              {t.insights.aiSummaryLastUpdated} {generatedLabel}
             </span>
           </div>
         </div>
